dd5b8273d9e3bc7190ccd069904feee37ac8ab67
Eng-Ahmed Farhat <ahmed8051741@gmail.com>
2025-11-06 11:40:35 +0200

    fix: Correct Gradle Kotlin DSL syntax - minifyEnabled to isMinifyEnabled

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
-            minifyEnabled = false
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
